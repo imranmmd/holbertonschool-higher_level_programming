6b6c92fa4c52f7b4e3d2f5969d955f7d5539575e
imranmmd <mr.imranmammadov@gmail.com>
2025-11-30 14:25:43 +0000

    Task #9 completed

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = -number if number < 0 else number
-    return number%10
+    last = number % 10 if number >= 0 else -number % 10
+    print(last, end="")
+    return last
