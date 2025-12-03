11c98f1e8028271c63bcdd886e602ca7756ab514
imranmmd <mr.imranmammadov@gmail.com>
2025-12-03 12:15:14 +0000

    Task #4 completed

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print(name)
-
+    for name in sorted(n for n in names if not n.startswith("__")):
+        print(name)
