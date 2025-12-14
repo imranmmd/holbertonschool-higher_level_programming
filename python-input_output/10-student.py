64cd54b9e11885f9067e877cfc885a337d3be516
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 20:11:22 +0000

    Task #10 completed

---
+++ python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a dictionaryof the Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        that exist in the instance are returned.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if isinstance(attr, str) and hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+
+        return self.__dict__
