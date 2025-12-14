6b0b2ee1b9f1894b54143002877fe26bf350b263
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 19:49:20 +0000

    Task #10 completed

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class with validated size and area calculation."""
+
+    def __init__(self, size):
+        """Initialize a Square with validated size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
