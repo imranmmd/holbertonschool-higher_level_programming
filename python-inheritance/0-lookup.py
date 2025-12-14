9d373d3ff5ff825f2312364b91b7e701ec064e4d
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 18:36:46 +0000

    Task #9 completed

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a function that returns the list of attributes and methods of an object."""
+
+
+def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
+    return dir(obj)
