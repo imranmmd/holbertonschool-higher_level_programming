cce2b6cff54f106f283ae3f77fed748d46cad134
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 19:57:29 +0000

    Task #1 completed

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return the number of characters."""
+    """Write a string to a UTF-8 text file.
+    Return the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
