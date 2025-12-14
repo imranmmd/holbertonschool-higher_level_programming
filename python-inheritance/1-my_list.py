5f09f32dc5ba30176ca9e79b139d71521727c114
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 18:41:55 +0000

    Task #1 completed

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,7 @@ class MyList(list):
     """Custom list class with a method to print a sorted version."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
-        print(sorted(self))
+        """Print the list sorted in ascending order and return it."""
+        sorted_list = sorted(self)
+        print(sorted_list)
+        return sorted_list
