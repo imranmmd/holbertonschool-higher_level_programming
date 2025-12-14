37206922b9f8315963ec4960ad56407a1caf9834
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 19:58:22 +0000

    Task #2 completed

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that appends text to a file."""
+
+
+def append_write(filename="", text=""):
+    """Append a string to a UTF-8 text file.
+    Return the number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
