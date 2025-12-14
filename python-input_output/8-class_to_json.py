cc36f88245c905d5817afd1ae85824b4492b3396
imranmmd <mr.imranmammadov@gmail.com>
2025-12-14 20:08:33 +0000

    Task #8 completed

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a function that returns a dictionary representation of an object."""
+"""Defines a function that returns a dict of an object."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization."""
+    """Return the dictionary  object for JSON serialization."""
     return obj.__dict__
