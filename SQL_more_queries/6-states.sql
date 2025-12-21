56902e0e861d26d65e256518b1f263cb6d579edd
imranmmd <mr.imranmammadov@gmail.com>
2025-12-21 16:51:27 +0000

    Task #6 completed

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- C+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
